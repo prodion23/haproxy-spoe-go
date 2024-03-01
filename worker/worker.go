01e4f5d7567b04e3dd45c866b8895f1244cf8441
Donald Propst <donnie@traceable.ai>
2024-03-01 15:27:38 -0500

    Make worker go func again

--- worker/worker.go
+++ worker/worker.go
@@ -139,7 +139,7 @@ func (w *worker) run() error {
 				return fmt.Errorf("worker not ready, but got Notify frame")
 			}
 
-			w.processNotifyFrame(f)
+			go w.processNotifyFrame(f)
 
 			// Time taken to start processNotifyFrame go routine
 			elapsedNotify := time.Since(notifyTimeStart)
